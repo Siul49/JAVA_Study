819d7d5315431ee1e2b4e7f699a3e307566ef70d
Siul <kksu149@gmail.com>
2025-05-12 02:48:36 +0900

    ch4 8번 문제 (아직 다 못품)

---
+++ src/ch4_practice/Account.java
@@ -0,0 +1,27 @@
+package ch4_practice;
+
+public class Account {
+
+    Account(int a){
+
+    }
+
+    public static void main(String[] args){
+        Account a = new Account(100);
+        a.deposit(5000);
+        System.out.println("잔금은 " + a.getBalance() + "원입니다.");
+
+        int bulk[] = {100, 500, 200, 700};
+        a.deposit(bulk);
+        System.out.println("잔금은 " + a.getBalance()  + "원입니다.");
+
+        int money = 1000;
+        int wMoney = a.withdraw(money);
+        if (wMoney < money)
+            System.out.println(wMoney + "원만 인출");
+        else
+            System.out.println(wMoney + "원 인출");
+
+        System.out.println("잔금은 " + a.getBalance() + "원입니다.");
+    }
+}
